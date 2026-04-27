ebed5f23f00dde10f801dec686e5d8973741ae35
fateh <fateh@gmail.com>
2026-04-27 22:47:27 +0100

    add in guezzm

--- jijel.py
+++ jijel.py
@@ -3,7 +3,7 @@ import os
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
-TARGET_CODES = ["16", "18"] # Algiers and Jijel
+TARGET_CODES = ["16", "18", "54"] # Algiers and Jijel
 
 def send_telegram_msg(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
